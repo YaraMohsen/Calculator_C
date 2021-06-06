bf07049ee9492db2709c26a4a45ee8def66a8209
yara mohsen <yaramohsen549@gmail.com>
2021-06-06 22:41:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,24 +4,13 @@
  *  Created on: Dec 11, 2020
  *      Author: SOUQ COMPUTER
  */
-//#include "memoryMap_avr32.h"
+
 #include "STD_Types.h"
 #include "BIT_MATH.h"
 #include "DIO_Reg.h"
 #include "DIO.h"
-//#include"ADC.h"
-//#include"SEVEN_SEG.h"
 #include"key_pad.h"
-//#include"sensorLDR.h"
 #include"calculator.h"
-
-//#include "EX_interrupt.h"
-
-#define F_CPU 8000000
-#include<util/delay.h>
-
-
-
 
 
 
@@ -31,15 +20,6 @@ int main()
 	DIO_Init();
 	LCD_Init();
 
-
-
-/*
-uint8 arr[15]="yara mohsen";
-uint8 i=0;
-for(i=0;arr[i]!='\0';i++){
-	LCD_WriteData(arr[i]);
-}
-*/
 
 
 	while(1){
